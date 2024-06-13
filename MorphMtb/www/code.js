2744428bb804fdc6e5b61e122ef2ed96b2a5b910
Lilreposurfer <elise.van.kerckhove@student.howest.be>
2024-06-13 10:00:32 +0200

    Change height and width of image

--- MorphMtb/www/code.js
+++ MorphMtb/www/code.js
@@ -1,4 +1,4 @@
 //place MORPH robot (logo) in navbar after loading page
 $( document ).ready(function() {
-  $( ".navbar .container-fluid" ).append( '<img src="MORPHrobot.png" align="right">');
+  $( ".navbar .container-fluid" ).append( '<img src="MORPHrobot.png" align="right" width="5%" height="5%">');
 });
